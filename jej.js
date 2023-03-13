5dabb4d8c64b6f366f8aac6df38b78a9800e2bd9
GregorySoldatman <grzegorzwojakowski0@gmail.com>
2023-03-13 12:35:29 +0100

    Gotowe

--- jej.js
+++ jej.js
@@ -1,33 +1,62 @@
-const modal = document.querySelector('#modal1')
-const opener = document.querySelector('.open')
-const cancel = document.querySelector('.modal__cancel')
-const confirmer = document.querySelector('.modal__confirm')
+const modal = document.querySelectorAll('.modal')
+const opener = document.querySelectorAll('.open-modal')
+const cancel = document.querySelectorAll('.modal__cancel')
+const confirmer = document.querySelectorAll('.modal__confirm')
+const content = document.querySelectorAll('.modal__content')
+let activeModal = null
 
-console.log(modal)
-console.log(opener)
-console.log(cancel)
-console.log(confirmer)
-
-opener.addEventListener('click', () => {
-    modal.style.display = 'block'
-})
-
-cancel.addEventListener('click', () => {
-    modal.style.display = 'none'
-})
-
-confirmer.addEventListener('click', () => {
-    console.log('Potwierdzono')
-})
-
-document.addEventListener('click', (event) => {
-    if (event.target == modal) {
-        modal.style.display = 'none'
+function openModal(id) {
+    const l = document.querySelector(id)
+    if (l == null) {
+        throw new Error('Błędne ID')
     }
-})
+    activeModal = id
+    l.style.removeProperty('display')
+    setTimeout(() => {l.classList.add('active')}, 1)
+}
+
+function hideModal(id) {
+    const l = document.querySelector(id)
+    if (l == null) {
+        throw new Error('Błędne ID')
+    }
+    activeModal = null
+    l.classList.remove('active')
+    setTimeout(() => {l.style.display = 'none'}, 200)
+}
+
+for (const element of opener) {
+    element.addEventListener('click', (event) => {
+        openModal(event.target.getAttribute('data-modal'))
+    })
+}
+
+for (const element of cancel) {
+    element.addEventListener('click', (event) => {
+        hideModal(event.target.getAttribute('data-modal'))
+    })
+}
+
+for (const element of confirmer) {
+    element.addEventListener('click', () => {
+        console.log('Potwierdzono')
+    })    
+}
+
+for (const element of modal) {
+    element.addEventListener('click', (event) => {
+        hideModal('#' + event.target.getAttribute('id'))
+    })
+}
+
+for (const element of content) {
+    element.addEventListener('click', (event) => {
+        event.stopPropagation()
+    })
+}
 
 document.addEventListener('keydown', (event) => {
-    if (event.key = 'Esc') {
-        modal.style.display = 'none'
+    if (activeModal != null && event.key == 'Escape') {
+        hideModal(activeModal)
     }
 })
